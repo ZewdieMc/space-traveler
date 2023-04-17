98a9a6c5ddcf826be70abfd4cc8f3aae893b45c1
ZewdieMc <zewdiehabtie26@gmail.com>
2023-04-17 22:33:29 +0300

    Style active class in NavLink

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,27 +1,45 @@
 import { NavLink, Link } from 'react-router-dom';
+import { useState } from 'react';
 import logo from '../images/planet_1.png';
 
-const NavBar = () => (
-  <header>
-    <nav>
-      <Link to="/">
-        <img className="logo" src={logo} alt="" />
-        <h1>Space traveler&apos; Hub</h1>
-      </Link>
-      <ul>
-        <NavLink className="nav-items" to="/">
-          Rockets
-        </NavLink>
-        <NavLink className="nav-items" to="/missions">
-          Missions
-        </NavLink>
-        <span>|</span>
-        <NavLink className="nav-items" to="/profile">
-          My Profile
-        </NavLink>
-      </ul>
-    </nav>
-  </header>
-);
+const NavBar = () => {
+  const [active, setActive] = useState(false);
+
+  return (
+    <header>
+      <nav>
+        <Link to="/">
+          <img className="logo" src={logo} alt="" />
+          <h1>Space traveler&apos; Hub</h1>
+        </Link>
+        <ul>
+          <NavLink
+            className={active === 'Rockets' ? 'active' : ''}
+            to="/"
+            onClick={() => setActive('Rockets')}
+          >
+            Rockets
+          </NavLink>
+          <span>|</span>
+          <NavLink
+            className={active === 'Missions' ? 'active' : ''}
+            to="/missions"
+            onClick={() => setActive('Missions')}
+          >
+            Missions
+          </NavLink>
+          <span>|</span>
+          <NavLink
+            className={active === 'Profile' ? 'active' : ''}
+            to="/profile"
+            onClick={() => setActive('Profile')}
+          >
+            My Profile
+          </NavLink>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default NavBar;
