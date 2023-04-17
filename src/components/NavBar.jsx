f97d7025dc9fabe86616741de3a48d5c6e637546
ZewdieMc <zewdiehabtie26@gmail.com>
2023-04-17 13:08:47 +0300

    Create the NavBar component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,27 @@
+import { NavLink, Link } from 'react-router-dom';
+import logo from '../images/planet.png';
+
+const NavBar = () => (
+  <header>
+    <nav>
+      <Link href="/">
+        <img className="logo" src={logo} alt="" />
+        <h1>Space traveler&apos; Hub</h1>
+      </Link>
+      <ul>
+        <NavLink className="nav-items" to="/rockets">
+          Rockets
+        </NavLink>
+        <NavLink className="nav-items" to="/missions">
+          Missions
+        </NavLink>
+        <span>|</span>
+        <NavLink className="nav-items" to="/profile">
+          My Profile
+        </NavLink>
+      </ul>
+    </nav>
+  </header>
+);
+
+export default NavBar;
