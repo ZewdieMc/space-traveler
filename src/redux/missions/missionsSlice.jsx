9e34ee564693747d82090e175c47fa58de25e33f
cherelemma <73167960+cherelemma@users.noreply.github.com>
2023-04-18 08:22:55 +0300

    Create slices for Missions

---
+++ src/redux/missions/missionsSlice.jsx
@@ -0,0 +1,36 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  missions: [{
+    id: "9D1B7E0",
+    name: "Thaicom",
+    description: "Thaicom is the name of a series of communications satellite, operated from Thailand."
+  },
+  {
+    id: "F4F83DE",
+    name: "Telstar",
+    description: "Telstar 19V (Telstar 19 Vantage) is a communication satellite in the Telstar series of the Canadian satellite communications company Telesat"
+  },
+  {
+    id: "F3364BF",
+    name: "Iridium NEXT",
+    description: "In 2017, Iridium began launching Iridium NEXT, a second-generation worldwide network of telecommunications satellites."
+  }
+],
+  isLoading: false,
+  error: '',
+};
+
+const missionsSlice = createSlice({
+  name: 'missions',
+  initialState,
+  reducers: {
+    fetchMissions: (state) => {
+        return state;
+    }
+  },
+  extraReducers: {},
+});
+
+export const { fetchMissions } = missionsSlice.actions;
+export default missionsSlice.reducer;
