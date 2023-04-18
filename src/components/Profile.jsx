f4b4c3fbbe719ca646b589352b0199faa4fe26f1
cherelemma <73167960+cherelemma@users.noreply.github.com>
2023-04-18 22:51:00 +0300

    Update Profile component and display joined missions

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,6 +1,35 @@
-const Profile = () => (
-  <div className="profile-wrapper">
-    <h3>Under Construction</h3>
-  </div>
-);
+import { useSelector } from 'react-redux';
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+
+function Profile() {
+  const JoinedMissions = useSelector((state) => {
+    const { missions } = state.missions;
+    return missions.filter((mission) => mission.reserved);
+  });
+
+  return (
+    <div className="profile-wrapper">
+      <div>
+        <h4>My Missions</h4>
+        <Card style={{ width: '18rem' }}>
+          {JoinedMissions.length ? JoinedMissions.map((mission) => (
+            <ListGroup key={mission.id} variant="flush">
+              <ListGroup.Item>{mission.mission_name}</ListGroup.Item>
+            </ListGroup>
+          ))
+            : (
+              <ListGroup>
+                <ListGroup.Item>No Missions Joined</ListGroup.Item>
+              </ListGroup>
+            )}
+        </Card>
+      </div>
+      <div>
+        <h3>My Rockets</h3>
+      </div>
+    </div>
+  );
+}
+
 export default Profile;
