5b4da8bd0cd947170d9e0698f9effa2e20e3ab17
ZewdieMc <zewdiehabtie26@gmail.com>
2023-04-18 20:38:53 +0300

    Dispatch booking action on button click

--- src/components/Rocket.jsx
+++ src/components/Rocket.jsx
@@ -1,19 +1,28 @@
 import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
+import { useDispatch } from 'react-redux';
+import { reserveRocket } from '../redux/rockets/rocketsSlice';
 
-const Rocket = ({ rocket }) => (
-  <div className="rocket">
-    <img src={rocket.flickr_images[0]} alt={rocket.rocket_name} />
-    <div className="rocket-detail">
-      <h2>{rocket.rocket_name}</h2>
-      <p>
-        {rocket.description}
-      </p>
-      <Button variant="primary">Reserve Rocket</Button>
+const Rocket = ({ rocket }) => {
+  const dispatch = useDispatch();
+
+  const handleReserverRocket = () => {
+    dispatch(reserveRocket(rocket.id));
+  };
+
+  return (
+    <div className="rocket">
+      <img src={rocket.flickr_images[0]} alt={rocket.rocket_name} />
+      <div className="rocket-detail">
+        <h2>{rocket.rocket_name}</h2>
+        <p>
+          {rocket.description}
+        </p>
+        <Button variant="primary" onClick={handleReserverRocket}>Reserve Rocket</Button>
+      </div>
     </div>
-  </div>
-);
-
+  );
+};
 Rocket.propTypes = {
   rocket: PropTypes.shape({
     id: PropTypes.string.isRequired,
