f2863a1efc6031c17cb122c1a87ffd4c1a34de21
cherelemma <73167960+cherelemma@users.noreply.github.com>
2023-04-18 08:25:23 +0300

    Add active link style

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,27 +1,46 @@
 import { NavLink, Link } from 'react-router-dom';
 import logo from '../images/planet_1.png';
 
-const NavBar = () => (
-  <header>
-    <nav>
-      <Link to="/">
-        <img className="logo" src={logo} alt="" />
-        <h1>Space traveler&apos; Hub</h1>
-      </Link>
-      <ul>
-        <NavLink className="nav-items" to="/">
-          Rockets
-        </NavLink>
-        <NavLink className="nav-items" to="/missions">
-          Missions
-        </NavLink>
-        <span>|</span>
-        <NavLink className="nav-items" to="/profile">
-          My Profile
-        </NavLink>
-      </ul>
-    </nav>
-  </header>
-);
+const NavBar = () => {
+  const navLinkStyle = ({ isActive }) => ({
+      fontWeight: isActive ? 'bold' : 'normal',
+      textDecoration: isActive ? 'underline' : 'none',
+});
+
+  return (
+    <header>
+      <nav>
+        <Link to="/">
+          <img className="logo" src={logo} alt="" />
+          <h1>Space traveler&apos; Hub</h1>
+        </Link>
+        <div>
+          <NavLink
+            className="nav-items"
+            to="/"
+            style={navLinkStyle}
+          >
+            Rockets
+          </NavLink>
+          <NavLink
+            className="nav-items"
+            to="/missions"
+            style={navLinkStyle}
+          >
+            Missions
+          </NavLink>
+          <span>|</span>
+          <NavLink
+            className="nav-items"
+            to="/profile"
+            style={navLinkStyle}
+          >
+            My Profile
+          </NavLink>
+        </div>
+      </nav>
+    </header>
+  );
+}
 
 export default NavBar;
