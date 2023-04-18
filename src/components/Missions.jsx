e56c0ce44f79ec8c510763fd0b7bc2f8f178e9fb
cherelemma <73167960+cherelemma@users.noreply.github.com>
2023-04-18 08:24:56 +0300

    Create Missions veiw component

---
+++ src/components/Missions.jsx
@@ -0,0 +1,38 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { FetchMissions } from '../redux/missions/missions';
+
+const Missions = () => {
+  const { missions } = useSelector((state) => state.missions);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(FetchMissions());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Mission</th>
+            <th>Description</th>
+            <th>Status</th>
+            <th> </th>
+          </tr>
+        </thead>
+        <tbody>
+          {missions.map((msn) => (
+            <tr key={msn.id} >
+                <td>msn.name</td>
+                <td>msn.description</td>
+                <td>Not Member</td>
+                <td>Join Mission</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Missions;
