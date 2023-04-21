09bcb6e0cc013355216c8e81c6e83a05d86d9ebd
cherelemma <73167960+cherelemma@users.noreply.github.com>
2023-04-21 13:01:13 +0300

    Add dragonReducer to root store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 import rocketsReducer from './rockets/rocketsSlice';
+import dragonsReducer from './dragons/dragonsSlice';
 import missionsReducer from './missions/missionsSlice';
 
 export default (preloadedState) => configureStore({
   reducer: {
     rockets: rocketsReducer,
+    dragons: dragonsReducer,
     missions: missionsReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
