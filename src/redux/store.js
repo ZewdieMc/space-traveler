229bd0ad07c797139add44922762605248271524
cherelemma <73167960+cherelemma@users.noreply.github.com>
2023-04-21 08:58:35 +0300

    Uncomment the redux-logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-// import logger from 'redux-logger';
+import logger from 'redux-logger';
 import rocketsReducer from './rockets/rocketsSlice';
 import missionsReducer from './missions/missionsSlice';
 
@@ -8,6 +8,6 @@ export default (preloadedState) => configureStore({
     rockets: rocketsReducer,
     missions: missionsReducer,
   },
-  // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
   preloadedState,
 });
