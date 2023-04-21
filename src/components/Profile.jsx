44da15f1476b266a31fdf788bbe8a43bbd5cb2c6
cherelemma <73167960+cherelemma@users.noreply.github.com>
2023-04-21 14:33:15 +0300

    Enhance Profile view component and add user interaction

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,9 +1,14 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
+import { Button } from 'react-bootstrap';
+import { reserveRocket } from '../redux/rockets/rocketsSlice';
+import { reserveDragon } from '../redux/dragons/dragonsSlice';
+import { joinMission } from '../redux/missions/missionsSlice';
 
 function Profile() {
-  const JoinedMissions = useSelector((state) => {
+  const dispatch = useDispatch();
+  const joinedMissions = useSelector((state) => {
     const { missions } = state.missions;
     return missions.filter((mission) => mission.reserved);
   });
@@ -14,14 +19,29 @@ function Profile() {
   const dragons = useSelector((state) => state.dragons.dragons);
   const reservedDragons = dragons.filter((dragon) => dragon.reserved);
 
+  const handleRocketReservation = (id) => dispatch(reserveRocket(id));
+  const handleDragonReservation = (id) => dispatch(reserveDragon(id));
+  const handleJoiningMissions = (id) => dispatch(joinMission(id));
+
   return (
     <div className="profile-wrapper">
       <div style={{ width: '100%' }}>
         <h4>My Missions</h4>
         <Card style={{ width: '100%' }}>
-          {JoinedMissions.length ? JoinedMissions.map((mission) => (
-            <ListGroup key={mission.id} variant="flush">
-              <ListGroup.Item>{mission.mission_name}</ListGroup.Item>
+          {joinedMissions.length ? joinedMissions.map((mission) => (
+            <ListGroup key={mission.id} horizontal style={{ alignItems: 'center' }}>
+              <ListGroup.Item style={{ width: '42%', border: 'none' }}>{mission.mission_name}</ListGroup.Item>
+              <ListGroup.Item style={{ margin: '5px', border: 'none', width: '28%' }}>
+                <Button
+                  size="sm"
+                  onClick={() => handleJoiningMissions(mission.id)}
+                >
+                  Leave Mission
+                </Button>
+              </ListGroup.Item>
+              <ListGroup.Item style={{ width: '30%', border: 'none' }}>
+                <a href={mission.wikipedia} rel="noreferrer" target="_blank">Read More</a>
+              </ListGroup.Item>
             </ListGroup>
           ))
             : (
@@ -35,8 +55,21 @@ function Profile() {
         <h4>My Rockets</h4>
         <Card style={{ width: '100%' }}>
           {reservedRockets.length ? reservedRockets.map((rocket) => (
-            <ListGroup key={rocket.id} variant="flush">
-              <ListGroup.Item>{rocket.name}</ListGroup.Item>
+            <ListGroup key={rocket.id} horizontal style={{ alignItems: 'center' }}>
+              <ListGroup.Item style={{ width: '42%', border: 'none' }}>
+                {rocket.name}
+              </ListGroup.Item>
+              <ListGroup.Item style={{ margin: '5px', border: 'none', width: '28%' }}>
+                <Button
+                  size="sm"
+                  onClick={() => handleRocketReservation(rocket.id)}
+                >
+                  Cancel Reservation
+                </Button>
+              </ListGroup.Item>
+              <ListGroup.Item style={{ width: '30%', border: 'none' }}>
+                <a href={rocket.wikipedia} rel="noreferrer" target="_blank">Read More</a>
+              </ListGroup.Item>
             </ListGroup>
           ))
             : (
@@ -50,8 +83,21 @@ function Profile() {
         <h4>My Dragons</h4>
         <Card style={{ width: '100%' }}>
           {reservedDragons.length ? reservedDragons.map((dragon) => (
-            <ListGroup key={dragon.id} variant="flush">
-              <ListGroup.Item>{dragon.name}</ListGroup.Item>
+            <ListGroup key={dragon.id} horizontal style={{ alignItems: 'center' }}>
+              <ListGroup.Item style={{ width: '42%', border: 'none' }}>
+                {dragon.name}
+              </ListGroup.Item>
+              <ListGroup.Item style={{ margin: '5px', border: 'none', width: '28%' }}>
+                <Button
+                  size="sm"
+                  onClick={() => handleDragonReservation(dragon.id)}
+                >
+                  Cancel Reservation
+                </Button>
+              </ListGroup.Item>
+              <ListGroup.Item style={{ width: '30%', border: 'none' }}>
+                <a href={dragon.wikipedia} rel="noreferrer" target="_blank">Read More</a>
+              </ListGroup.Item>
             </ListGroup>
           ))
             : (
